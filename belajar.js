94ee5e5a0b596a6f7d9ac2ce7ddebf641f04b6b6
Ricmaw <rickydarmawan040901@gmail.com>
2023-12-19 18:38:01 +0700

    challenge pertama

---
+++ belajar.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+// code ini berfungsi untuk berkata hello world
+
+var namaLengkap = "Ricky";
+var alamat = `Palembang`;
+
+console.log(`Nama saya ${namaLengkap} alamat saya di ${alamat}`);
+
+let merkMobil1 = "Nissan"
+let merkMobil2 = "Toyota"
+let merkMobil3 = "Honda"
+
+console.log(merkMobil1, merkMobil2, merkMobil3)
+
+let data = "nama saya budi"
+let hasil = data.replace("budi", "surya")
+
+console.log(hasil)
+
